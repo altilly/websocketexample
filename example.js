71cd07d5af336d6deb8c1fa227979b0ef5f9fcda
altilly <42125809+altilly@users.noreply.github.com>
2019-08-02 18:06:55 +0800

    Update example.js

--- example.js
+++ example.js
@@ -4,6 +4,9 @@ const ws = new WebSocket('wss://wsapi.altilly.com:2096');
 
 ws.on('open', function open() {
   console.log('connected');
+  
+  doStuff();
+  
 });
 
 ws.on('close', function close() {
@@ -14,5 +17,8 @@ ws.on('message', function incoming(data) {
   console.log(data);
 });
 
-ws.send({"method":"subscribeOrderbook","params":{"symbol":"XQRBTC"}});
-ws.send({"method":"subscribeOrderbook","params":{"symbol":"ODINBTC"}});
+function doStuff()
+{
+  ws.send({"method":"subscribeOrderbook","params":{"symbol":"XQRBTC"}});
+  ws.send({"method":"subscribeOrderbook","params":{"symbol":"ODINBTC"}});
+}
